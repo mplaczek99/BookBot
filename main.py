911d525dd940fc9eaee754d8d4a722c1f686e105
mplaczek99 <mplaczek99@gmail.com>
2026-02-08 14:31:29 -0600

    Renamed & Moved word counter function to stats.py

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
+from stats import get_num_words
+
 def get_book_texts(filepath):
     with open(filepath) as f:
         file_contents = f.read()
         return file_contents
 
-def word_counter(text):
-    num_words = len(text.split())
-    print(f"Found {num_words} total words")
-    return num_words
-
 def main():
     text = get_book_texts("books/frankenstein.txt")
-    word_counter(text)
+    get_num_words(text)
 
 main()
---
+++ stats.py
@@ -0,0 +1,4 @@
+def get_num_words(text):
+    num_words = len(text.split())
+    print(f"Found {num_words} total words")
+    return num_words
