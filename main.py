370ff84ba59eabc89ef068801d3d35c67d1b2a6d
mplaczek99 <mplaczek99@gmail.com>
2026-02-08 16:04:44 -0600

    Added function to sort characters, also did some tidying with names

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
-from stats import get_num_words, get_num_characters
+from stats import get_num_words, get_characters, sort_characters
 
 def get_book_texts(filepath):
+    print(f"Analyzing book found at {filepath}...")
     with open(filepath) as f:
         file_contents = f.read()
         return file_contents
 
 def main():
+    print("============ BOOKBOT ============")
     text = get_book_texts("books/frankenstein.txt")
 
     num_words = get_num_words(text)
+    print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
 
-    characters = get_num_characters(text)
-    print(characters)
+    print("--------- Character Count -------")
+    characters = get_characters(text)
+    sorted_characters = sort_characters(characters)
+    for character in sorted_characters:
+        print(f"{character['char']}: {character['num']}")
+
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -2,7 +2,7 @@ def get_num_words(text):
     num_words = len(text.split())
     return num_words
 
-def get_num_characters(text):
+def get_characters(text):
     characters = {}
     lower_text = text.lower()
     for char in lower_text:
@@ -11,3 +11,16 @@ def get_num_characters(text):
         else:
             characters[char] += 1
     return characters
+
+def sort_characters(characters):
+    sorted_list = []
+
+    for char, count in characters.items():
+        if char.isalpha(): # Ignore spaces/punctuation
+            sorted_list.append({"char": char, "num": count})
+
+    sorted_list.sort(reverse=True, key=find_num)
+    return sorted_list
+
+def find_num(characters):
+    return characters["num"]
