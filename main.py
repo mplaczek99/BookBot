9f41964d8ed4632991564ad5f55cf32c56ebec71
mplaczek99 <mplaczek99@gmail.com>
2026-02-08 14:02:57 -0600

    Made a function to read the book

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_texts(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+        return file_contents
+
+def main():
+    text = get_book_texts("books/frankenstein.txt")
+    print(text)
+
+main()
